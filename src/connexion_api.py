de752bbd7cbb18200b9b38fea124cb259a040133
Théo Laperrouse <theolaperrouse@gmail.com>
2023-02-15 09:49:08 +0100

    Fix connexion API

--- src/connexion_api.py
+++ src/connexion_api.py
@@ -9,7 +9,6 @@ from dotenv import dotenv_values
 
 config = dotenv_values(".env")
 
-
 def sign_hmac_sha1(key, time_str):
     '''Sign with sha1'''
     hmac_object = hmac.new(key.encode(
@@ -23,15 +22,13 @@ def connexion_api(api, params=None):
     key = hashlib.md5(config['KEY_FFTT'].encode('utf-8')).hexdigest()
     serie = ''.join(random.choices(
         string.ascii_letters + string.digits, k=15)).upper()
-    timestamp = round(time.time() * 1000)
-    tmc = sign_hmac_sha1(key, str(time))
-    url = f"http://www.fftt.com/mobile/pxml/{api}.php" \
-        f"?serie={serie}&tm={timestamp}&tmc={tmc}&id={id_fftt}"
+    timestamp = int(round(time.time() * 1000))
+    tmc = sign_hmac_sha1(key, str(timestamp))
+    url = f"http://www.fftt.com/mobile/pxml/{api}.php?serie={serie}&tm={timestamp}&tmc={tmc}&id={id_fftt}"
     if params is not None:
         url = f"{url}&{params}"
     try:
-        response = requests.get(url)
+        response = requests.get(url, timeout=60)
         return xmltodict.parse(response.text)['liste']
     except requests.exceptions.RequestException as error:
-        print(error)
         return None
