03348be1160bbdc4f8eaaa5564965863ddc7d0d6
Théo Laperrouse <theolaperrouse@gmail.com>
2023-03-13 23:29:55 +0100

    Fix deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,5 +3,5 @@ git pull
 sudo docker rm -f fastapi_fftt
 sudo docker rm -f redis
 sudo docker build . -t fastapi_fftt
-sudo docker run --name redis -p 6379 -d redis 
+sudo docker run -d --name redis -p 6379 redis 
 sudo docker run -d -p 80:8000 --name fastapi_fftt fastapi_fftt
